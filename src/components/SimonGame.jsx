c97ab6689c7036e6f759e163992c1dfb2850b43e
Karma0o7 <23173amitkanand@gmail.com>
2022-11-26 17:22:05 +0530

    Completed

--- src/components/SimonGame.jsx
+++ src/components/SimonGame.jsx
@@ -7,6 +7,7 @@ function SimonGame() {
   // states
   const [sequence, setSequence] = useState([]);
   const [playing, setPlaying] = useState(false);
+  const [playingIdx, setPlayingIdx] = useState(0);
 
   // refs
   const greenRef = useRef(null);
@@ -15,6 +16,12 @@ function SimonGame() {
   const blueRef = useRef(null);
 
   // Functions
+  const resetGame = () => {
+    setSequence([]);
+    setPlaying(false);
+    setPlayingIdx(0);
+  }
+
   const addNewcolor = () => {
     const color = colors[Math.floor(Math.random() * colors.length)];
     const newSequence = [...sequence, color];
@@ -23,65 +30,128 @@ function SimonGame() {
 
   const handelNextLevel = () => {
     if (!playing) {
+      setPlaying(true);
       addNewcolor();
     }
   };
 
+  const handelColorClick = (e) => {
+    if (playing) {
+      e.target.classList.add("opacity-50");
+      setTimeout(() => {
+        e.target.classList.remove("opacity-50");
+
+        const clickColor = e.target.getAttribute("color");
+        // Clicked the correct color of the sequence
+        if(sequence[playingIdx] === clickColor) {
+          // Clicked the last color of the sequence
+          if(playingIdx === sequence.length - 1) {
+            setTimeout(() => {
+              setPlayingIdx(0);
+              addNewcolor();
+            }, 250);
+          }
+  
+          // missing some colors of the sequence to be clicked
+          else {
+            setPlayingIdx(playingIdx + 1);
+          }
+        }
+  
+        // Clicked the wrong color of the sequence
+        else{
+          resetGame()
+          // alert("You Lost!");
+        }
+      }, 250);
+
+
+    }
+  }
+
   //useEffect
   useEffect(() => {
     //show sequence
-    const showSequence = (idx = 0) => {
-      let ref = null;
-
-      if(sequence[idx] === "green") ref = greenRef;
-      if(sequence[idx] === "red") ref = redRef;
-      if(sequence[idx] === "yellow") ref = yellowRef;
-      if(sequence[idx] === "blue") ref = blueRef;
-      
-      // highlight the ref
-      setTimeout(() => {
-        ref.current.classList.add("brightness-[2.5]");
-
+    if (sequence.length > 0) {
+      const showSequence = (idx = 0) => {
+        let ref = null;
+  
+        if(sequence[idx] === "green") ref = greenRef;
+        if(sequence[idx] === "red") ref = redRef;
+        if(sequence[idx] === "yellow") ref = yellowRef;
+        if(sequence[idx] === "blue") ref = blueRef;
+        
+        // highlight the ref
         setTimeout(() => {
-          ref.current.classList.remove("brightness-[2.5]");
-          if (idx  < sequence.length - 1) showSequence(idx + 1);
+          ref.current.classList.add("brightness-[2.5]");
+  
+          setTimeout(() => {
+            ref.current.classList.remove("brightness-[2.5]");
+            if (idx  < sequence.length - 1) showSequence(idx + 1);
+          }, 250);
+  
         }, 250);
-
-      }, 250);
-    };
-
-    showSequence();
+      };
+  
+      showSequence();
+    }
   }, [sequence]);
 
 
   return (
     // Main container
+    
     <div className="flex justify-center items-center bg-neutral-800 text-white w-screen h-screen">
       {/* Game container */}
       <div className="relative flex flex-col justify-center items-center">
+        <h1 className="pb-10 text-3xl font-bold">Simon Game</h1>
         {/* Green and red container */}
         <div>
           {/* Green button */}
-          <GameBtn border="rounded-tl-full" bg="bg-green-500" ref={greenRef} />
+          <GameBtn 
+            color="green" 
+            border="rounded-tl-full" 
+            bg="bg-green-500" 
+            onClick={handelColorClick} 
+            ref={greenRef} 
+          />
 
           {/* Red button */}
-          <GameBtn border="rounded-tr-full" bg="bg-red-500" ref={redRef} />
+          <GameBtn 
+            color="red" 
+            border="rounded-tr-full" 
+            bg="bg-red-500" 
+            onClick={handelColorClick} 
+            ref={redRef}
+          />
         </div>
 
         {/* Yellow and Bluw container */}
         <div>
           {/* Yellow button */}
-          <GameBtn border="rounded-bl-full" bg="bg-yellow-400" ref={yellowRef} />
+          <GameBtn 
+            color="yellow" 
+            border="rounded-bl-full" 
+            bg="bg-yellow-400" 
+            onClick={handelColorClick} 
+            ref={yellowRef} 
+          />
 
           {/* Blue button */}
-          <GameBtn border="rounded-br-full" bg="bg-blue-500" ref={blueRef} />
+          <GameBtn 
+            color="blue" 
+            border="rounded-br-full" 
+            bg="bg-blue-500" 
+            onClick={handelColorClick} 
+            ref={blueRef} 
+          />
         </div>
 
         {/* Play button */}
         <button 
-          className="absolute bg-neutral-900 text-white text-xl sm:text-2xl font-bold rounded-full w-[150px] sm:w-[175px] h-[150px] sm:h-[175px] duration-200 hover:scale-105" 
+          className="absolute mt-12 bg-neutral-900 text-white text-xl sm:text-2xl font-bold rounded-full w-[150px] sm:w-[175px] h-[150px] sm:h-[175px] duration-200 hover:scale-105" 
           onClick={handelNextLevel}>
-          Play
+          {sequence.length === 0 ? "Play" : sequence.length}
         </button>
       </div>
     </div>
